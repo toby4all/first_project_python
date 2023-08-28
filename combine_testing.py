63d0a1fc3967d31af0fd3905c9e4f881850c931a
toby4all <toby4all@gmail.com>
2023-08-29 01:23:27 +0200

    added clean environment for pipeline and pipeline was structured

--- combine_testing.py
+++ combine_testing.py
@@ -5,17 +5,16 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 # request to post new data to database with usr_name 'John'
-res = requests.post('http://127.0.0.1:5000/users/3', json={"name": "Oluwatobi"})
-if res.ok:
-    print(res.status_code)
+res = requests.post('http://127.0.0.1:5000/users/6', json={"name": "Victoria"})
+if res.status_code == 200:
     print(res.json())
 
-# request to get data from database with usr_id '2'
-response = requests.get('http://127.0.0.1:5000/users/get_user_data/2')
-if response.ok:
-    print(response.status_code)
+# request to get data from database with usr_id '6'
+response = requests.get('http://127.0.0.1:5000/users/6')
+if response.status_code == 200:
+    print(response.text)
 
-# query database to make sure username is stored under id:'1'
+# query database to make sure username is stored under id:'6'
 
 schema_name = "mydb"
 
@@ -25,22 +24,24 @@ conn = pymysql.connect(host='127.0.0.1', port=3306, user='user', passwd='passwor
 cursor = conn.cursor()
 
 # Getting all data from table “users”
-cursor.execute(f"SELECT * FROM {schema_name}.users WHERE user_id ='2' ")
+cursor.execute(f"SELECT * FROM {schema_name}.users WHERE user_id ='6' ")
 
-# Iterating table and printing all users
+# Iterating table to print the user details on the row with this user_id.
 for row in cursor:
     print(row)
 
 cursor.close()
 conn.close()
 
-driver = webdriver.Chrome(service=Service("C:/Users/Toby/Desktop/Global dev ops/chromedriver.exe"))
+driver = webdriver.Chrome(service=Service("C:/Users/Toby/Desktop/Global dev ops/chromedriver-win64/chromedriver-win64/chromedriver.exe"))
 driver.implicitly_wait(10)
-driver.get('http://127.0.0.1:5000/users/2')
+driver.get('http://127.0.0.1:5001/users/get_user_data/1')
 print(driver.current_url)
+try:
+    elements = driver.find_element(By.ID, 'user')
+    print(elements.text)
+except Service:
+    print('No such user')
+finally:
+    print(elements.text)
 
-elements = driver.find_element(By.ID, 'user')
-print(elements.text)
-
-elements = driver.find_element(By.ID, 'no-user')
-print(elements.text)

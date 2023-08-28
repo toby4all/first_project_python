fdcd4f9a667e4e2b3e719ad1bc1b1b2eb8a80208
toby4all <toby4all@gmail.com>
2023-08-29 01:24:31 +0200

    added clean environment for pipeline and pipeline was structured

--- web_rest.py
+++ web_rest.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from db_connector import get_user
+import os
+import signal
 
 app = Flask(__name__)
 
@@ -12,7 +14,12 @@ def getuser(user_id):
         else:
             return "<H1 id = 'no-user'>"'No such user with this id: ' + user_id + "</H1>"
 
+@app.route("/stop_server")   # route to stop server
+def stop_server():
+    os.kill(os.getpid(), signal.CTRL_C_EVENT)
+    return 'Server stopped'
+
 # host is pointing at local machine address
 # debug is used for more detailed logs + hot swaping
 # the desired port - feel free to change
-app.run(host='127.0.0.1', debug=True, port=5000)
+app.run(host='127.0.0.1', debug=True, port=5001)

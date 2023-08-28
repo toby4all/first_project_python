38dff1249eec447c27bcb6e6facc82f7aa805bdd
toby4all <toby4all@gmail.com>
2023-08-29 01:25:10 +0200

    added clean environment for pipeline and pipeline was structured

--- web_interface.py
+++ web_interface.py
@@ -1,11 +1,10 @@
 import requests
 
-# res = requests.get('http://127.0.0.1:5000/users/get_user_data/2')
-# if res.ok:
-#     print(res.status_code)
+res = requests.get('http://127.0.0.1:5001/users/get_user_data/2')
+if res.ok:
+    print(res.status_code)
 
 response = requests.put('http://127.0.0.1:5000/users/2', json={"name": "George"})
 user_id= 2
 if response.status_code == 200:
-    # print(response.status_code)
     print(response.text)
